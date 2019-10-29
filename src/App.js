9980db9996dfb6120621f07f1154537fadc8ee2a
JP Sio <sio.jeanpaul@gmail.com>
2019-10-28 19:44:38 -0700

    Get money, get paid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import {
+  differenceInCalendarDays,
+  format,
+  isSaturday,
+  isSunday,
+  lastDayOfMonth
+} from "date-fns";
+
+import "./App.css";
+
+export function isPastMidMonth(dayOfMonth) {
+  return dayOfMonth > 15;
+}
+
+export function midDayOfMonth(today) {
+  return new Date(+format(today, "Y"), +format(today, "M") - 1, 15);
+}
+
+export function getNextPayDay(today) {
+  return isPastMidMonth(+format(today, "d"))
+    ? lastDayOfMonth(today)
+    : midDayOfMonth(today);
+}
+
+export function daysUntilPaycheck(today) {
+  const payDay = getNextPayDay(today);
+  const difference = differenceInCalendarDays(payDay, today);
+
+  if (isSaturday(payDay)) {
+    return difference - 1;
+  } else if (isSunday(payDay)) {
+    return difference - 2;
+  } else {
+    return difference;
+  }
+}
 
 function App() {
+  const days = daysUntilPaycheck(new Date());
+
+  useEffect(() => {
+    document.title = `${days} Days Till Next Paycheck`;
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {days === 0
+          ? <h2>It's Pay Day!</h2>
+          : <h1 className="App-title">
+              {days}
+            </h1>}
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { daysUntilPaycheck } from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("daysUntilPaycheck", () => {
+  it("returns 0 on actual pay day!", () => {
+    const today = new Date(2019, 9, 15);
+    const actual = daysUntilPaycheck(today);
+    expect(actual).toEqual(0);
+  });
+
+  it("calculates correctly on the day after the fist paycheck", () => {
+    const today = new Date(2019, 9, 16);
+    const actual = daysUntilPaycheck(today);
+    expect(actual).toEqual(15);
+  });
+
+  it("calculates from Friday when payday lands on Saturday", () => {
+    const today = new Date(2020, 1, 10);
+    const actual = daysUntilPaycheck(today);
+    expect(actual).toEqual(4);
+  });
+
+  it("returns 0 on the Friday before payday when payday is on the weekend", () => {
+    const today = new Date(2020, 1, 14);
+    const actual = daysUntilPaycheck(today);
+    expect(actual).toEqual(0);
+  });
+
+  it("calculates from Friday when payday lands on Sunday", () => {
+    const today = new Date(2020, 2, 2);
+    const actual = daysUntilPaycheck(today);
+    expect(actual).toEqual(11);
+  });
 });
